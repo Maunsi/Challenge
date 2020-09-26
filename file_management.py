048708b0755173140805340969838541f5c87e6f
Maureen Keegan <maukeegan@Maureens-MacBook-Pro.local>
2020-09-25 21:25:03 -0300

    Cutting columns before sorting

--- file_management.py
+++ file_management.py
@@ -7,11 +7,9 @@ def download_file(url, destination):
        file.write(customer_file.content)
 
 def read_file_as_dataframe(file):
-    # TODO: do something to avoid reading entire file to memory
     with open(file, 'r') as customer_file:
         customers = pd.read_json(customer_file, lines=True)
     return customers
 
 def write_output(customers, destination):
-    #TODO: not sure we want a csv
     customers.to_csv(destination, index=False)
--- select_customers.py
+++ select_customers.py
@@ -6,22 +6,25 @@ def check_columns(customers):
     if not expected_columns.issubset(customers.columns):
         raise Exception("Invalid file, columns missing")
 
-
 def select_customers(customers, office, distance):
     """
-    :param customers: dataframe containing the customers, should have the following columns: 'latitude', 'longitude', 'user_id', 'name'
+    :param customers: DataFrame containing the customers, should have the following columns: 'latitude', 'longitude', 'user_id', 'name'
     :param office: latitude and longitude of the office
     :param distance: max distance to the office in km
-    :return: dataframe with user id and name of the selected customers.
+    :return: DataFrame with user id and name of the selected customers.
     """
     if customers.empty:
         return pd.DataFrame(columns=["user_id", "name"])
 
     check_columns(customers)
+
     customers['distance_to_office'] = customers.apply(
         lambda row: great_circle((row['latitude'], row['longitude']), office).kilometers, axis=1)
 
     customers_within_distance = customers.loc[customers['distance_to_office'] < distance]
 
-    sorted_customers = customers_within_distance.sort_values(by='user_id')
-    return sorted_customers.loc[:, ["user_id", "name"]]
+    customers_id_name = customers_within_distance.loc[:, ["user_id", "name"]]
+
+    sorted_customers = customers_id_name.sort_values(by='user_id')
+
+    return sorted_customers
